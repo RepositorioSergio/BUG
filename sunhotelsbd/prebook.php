4236c334be8a01842112c4369c3cd809ae2c52dc
SergioSantos <sergio.m.d.santos@gmail.com>
2019-12-09 00:33:57 +0000

    SunHotelsbd V1

---
+++ sunhotelsbd/prebook.php
@@ -0,0 +1,94 @@
+<?php
+require '../vendor/autoload.php';
+use Zend\Db\Sql\Sql;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Metadata;
+use Zend\Http\Client;
+use Zend\Http\Request;
+use Zend\Json\Json;
+use Zend\Config;
+use Zend\Log\Logger;
+use Zend\Log\Writer;
+echo "COMECOU PREBOOKING<br/>";
+if (! $_SERVER['DOCUMENT_ROOT']) {
+    // On Command Line
+    $return = "\r\n";
+} else {
+    // HTTP Browser
+    $return = "<br>";
+}
+$config = new \Zend\Config\Config(include '../config/autoload/global.php');
+$config = [
+    'driver' => $config->db->driver,
+    'database' => $config->db->database,
+    'username' => $config->db->username,
+    'password' => $config->db->password,
+    'hostname' => $config->db->hostname
+];
+$db = new \Zend\Db\Adapter\Adapter($config);
+// Start
+$affiliate_id = 0;
+$branch_filter = "";
+
+
+$config = new \Zend\Config\Config(include '../config/autoload/global.abreu.php');
+$config = [
+    'driver' => $config->db->driver,
+    'database' => $config->db->database,
+    'username' => $config->db->username,
+    'password' => $config->db->password,
+    'hostname' => $config->db->hostname
+];
+
+date_default_timezone_set('UTC');
+//$date = date("Y-m-d H:i:s");
+$date = new DateTime();
+$date = $date->format("Y-m-d H:i:s");
+
+$url = "http://xml.sunhotels.net/15/PostGet/NonStaticXMLAPI.asmx/PreBookV2?userName=BugSofttest&password=bkGk2sWP&currency=EUR&language=en&checkInDate=2017-11-23&checkOutDate=2017-11-25&roomId=33513&rooms=1&adults=1&children=1&childrenAges=5&infant=0&mealId=3&CustomerCountry=gb&B2C=0&searchPrice=100&showPriceBreakdown=true";
+
+$headers = array(
+    'Accept-Encoding: gzip,deflate'
+); 
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_URL, $url . '/activity.json/search?currency=USD&lang=EN');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_ENCODING , "gzip,deflate");
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $raw);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$response = curl_exec($ch);
+$error = curl_error($ch);
+$headers = curl_getinfo($ch);
+curl_close($ch);
+
+echo "<xmp>";
+echo $response;
+echo "</xmp>";
+
+die();
+$config = new \Zend\Config\Config(include '../config/autoload/global.abreu.php');
+$config = [
+    'driver' => $config->db->driver,
+    'database' => $config->db->database,
+    'username' => $config->db->username,
+    'password' => $config->db->password,
+    'hostname' => $config->db->hostname
+];
+$db = new \Zend\Db\Adapter\Adapter($config);
+
+
+
+
+// EOF
+$db->getDriver()
+    ->getConnection()
+    ->disconnect();
+echo 'Done';
+?>
